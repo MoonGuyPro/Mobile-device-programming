7a18bc1477aefa68ed903c9aaa16c533df296e8d
Wiktor <73667681+MoonGuyPro@users.noreply.github.com>
2024-04-02 23:45:35 +0200

    feat: Classes added to ViewModel

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.Interfaces;
+using Data.Models;
+
+namespace ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+    }
+}
---
+++ ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    // Klasa bazowa dla ViewModeli, implementująca interfejs INotifyPropertyChanged
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
