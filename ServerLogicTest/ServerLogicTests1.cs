18d575f8945b350080c767a83dc012a55ed818e9
Karol D <karold@tutanota.com>
2024-04-15 03:10:57 +0200

    feat: added server logic tests

---
+++ ServerLogicTest/ServerLogicTests1.cs
@@ -0,0 +1,86 @@
+using ServerData;
+using ServerLogic;
+namespace ServerLogicTest
+{
+    public class CandidateRepositoryMock : CandidateRepositoryAbstract
+    {
+        private readonly CandidateRepoMock repoMock = new CandidateRepoMock();
+
+        public override ICandidateRepository GetCandidateRepository()
+        {
+            return repoMock;
+        }
+    }
+
+    public class CandidateMock : ICandidateModel
+    {
+        public CandidateMock(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+
+
+    public class CandidateRepoMock : ICandidateRepository
+    {
+        private readonly List<ICandidateModel> allCandidates;
+
+        public CandidateRepoMock()
+        {
+            allCandidates = new List<ICandidateModel>
+            {
+                new CandidateMock(1,"Candidate 1"),
+                new CandidateMock(2,"Candidate 2"),
+                new CandidateMock(3,"Candidate 3")
+            };
+
+        }
+        public void AddCandidate(int id, string name)
+        {
+            var existingCandidate = allCandidates.FirstOrDefault(c => c.Id == id);
+            if (existingCandidate != null)
+            {
+                throw new ArgumentException($"A candidate with ID {id} already exists.");
+            }
+
+            allCandidates.Add(new CandidateMock(id, name));
+        }
+
+        public List<ICandidateModel> GetAllCandidates()
+        {
+            return allCandidates;
+        }
+
+        public void RemoveCandidate(int id)
+        {
+            var candidateToRemove = allCandidates.FirstOrDefault(c => c.Id == id);
+            if (candidateToRemove == null)
+            {
+                throw new ArgumentException($"A candidate with ID {id} does not exist.");
+            }
+
+            allCandidates.Remove(candidateToRemove);
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private ElectionServiceAbstract ESAbstract = ElectionServiceAbstract.Create(new CandidateRepositoryMock());
+
+        [TestMethod]
+        public void CheckCandidate()
+        {
+            //System.Diagnostics.Debug.WriteLine($"Voted for candidate ID: {ESAbstract.GetCandidates().GetCandidates().Count}");
+
+            Assert.IsTrue(ESAbstract.GetCandidates().GetCandidates().Count == 3);
+            Assert.IsTrue(ESAbstract.GetCandidates().GetCandidates().First().Id == 1);
+            Assert.IsTrue(ESAbstract.GetCandidates().GetCandidates().First().Name == "Candidate 1");
+            Assert.IsTrue(ESAbstract.GetCandidates().GetCandidates().Last().Id == 3);
+            //ESAbstract.GetCandidates().GetCandidates();
+        }
+    }
+}
