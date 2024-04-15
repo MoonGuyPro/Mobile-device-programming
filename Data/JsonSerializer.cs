87f3662033bbd8879538239b25eb319ca5e272a9
Karol D <karold@tutanota.com>
2024-04-15 03:21:12 +0200

    feat: installed Newtonsoft.Json and added basic serializer to the client data layer

---
+++ Data/JsonSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ClientData
+{
+	internal class JsonSerializer : Serializer
+	{
+		public override string Serialize<T>(T objectToSerialize)
+		{
+			return JsonConvert.SerializeObject(objectToSerialize);
+		}
+
+		public override T Deserialize<T>(string message)
+		{
+			return JsonConvert.DeserializeObject<T>(message);
+		}
+
+		public override string? GetResponseHeader(string message)
+		{
+			JObject jObject = JObject.Parse(message);
+			if (jObject.ContainsKey("Header"))
+			{
+				return (string)jObject["Header"];
+			}
+
+			return null;
+		}
+	}
+}
